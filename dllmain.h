528ea220400804044416b07a53bfaa43d599ac5f
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2022-06-14 17:02:29 -0400

    Cleaned stuff up a little

---
+++ dllmain.h
@@ -0,0 +1,38 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <MinHook.h>
+#include <Psapi.h>
+#include <cassert>
+#include <cstdint>
+#include <map>
+#include "include/imgui/imgui.h"
+#include "include/imgui/imgui_impl_win32.h"
+#include "include/imgui/imgui_impl_dx12.h"
+#include "include/imgui/imgui_impl_dx11.h"
+#include <initguid.h>
+#include <dxgi.h>
+#include <d3d11.h>
+#include <d3d12.h>
+#include <d2d1_1.h>
+#include <dxgi1_4.h>
+#include <dwrite_1.h>
+#include "include/kiero/kiero.h"
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dwrite.lib")
+#include "Utils/Utils.h"
+#include "Utils/HookImgui.h"
+#include "Utils/ClientLogs.h"
+bool clientAlive = true;
+bool initImgui = true;
+bool renderClickUI = false;
+
+//Keymap and key hook
+std::map<uint64_t, bool> keymap = std::map<uint64_t, bool>();
+typedef void(__thiscall* key)(uint64_t keyId, bool held);
+key _key;
+
+//Mouse hook
+typedef void(__thiscall* Mouse)(__int64 a1, char mouseButton, char isDown, __int16 mouseX, __int16 mouseY, __int16 relativeMovementX, __int16 relativeMovementY, char a8);
+Mouse _Mouse;
