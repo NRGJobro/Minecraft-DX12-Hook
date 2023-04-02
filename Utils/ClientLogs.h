9f3f8a91b6c37bd12a0e763558422300e2f7a7ff
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2023-04-02 19:31:31 -0400

    Test commit

--- Utils/ClientLogs.h
+++ Utils/ClientLogs.h
@@ -7,11 +7,5 @@
 class Logger {
 public:
     static void log(std::string logMsg) {
-        /*
-        std::string path = getenv("APPDATA") + std::string("\\..\\Local\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\RoamingState\\MC_Dx12_Logs.txt");
-        std::ofstream ofs(path.c_str(), std::ios_base::out | std::ios_base::app);
-        ofs << logMsg << '\n';
-        ofs.close();
-        */
     };
 };
